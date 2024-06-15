5602e328121a7745560dc45827ec66a1ccf8dc06
Coffee Coke <169088425+ykk2b@users.noreply.github.com>
2024-06-16 00:49:15 +0400

    add new commands and directory playing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::{App, Arg};
 use rodio::{Decoder, OutputStream, Sink, Source};
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{stdin, BufReader};
+use std::path::{Path, PathBuf};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -23,24 +24,74 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn shuffle_files(files: &mut [PathBuf]) {
+    let n = files.len();
+    for i in (0..n).rev() {
+        let j = rand::random::<usize>() % (i + 1);
+        files.swap(i, j);
+    }
+}
 async fn play_audio(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
-    let file = File::open(file_path).unwrap();
-    let reader = BufReader::new(file);
+    let is_dir = Path::new(file_path).is_dir();
+    let path = Path::new(file_path);
+    let mut shuffle = true;
+    if is_dir {
+        let mut files = Vec::new();
+        for entry in fs::read_dir(file_path)? {
+            let entry = entry?;
+            let path = entry.path();
 
-    let source = Decoder::new(reader)
-        .map_err(|e| {
-            eprintln!("Error decoding audio file: {}", e);
-            e
-        })
-        .unwrap()
-        .pausable(false)
-        .repeat_infinite();
+            if path.extension().and_then(|s| s.to_str()) == Some("mp3") {
+                files.push(path);
+            }
+        }
+        if shuffle {
+            shuffle_files(&mut files);
+        }
 
-    sink.append(source);
-    sink.play();
-    println!("Type 'help' to get the list of commands. Type 'exit' to stop the player.");
+        for file in &files {
+            let file = File::open(file)?;
+            let reader = BufReader::new(file);
+
+            let source = Decoder::new(reader)
+                .map_err(|e| {
+                    eprintln!("Error decoding audio file: {}", e);
+                    e
+                })?
+                .pausable(false);
+
+            sink.append(source);
+        }
+
+        sink.play();
+        if let Some(file_name) = files
+            .get(0)
+            .and_then(|p| p.file_name())
+            .and_then(|n| n.to_str())
+        {
+            println!("Playing: {}", file_name);
+        }
+    } else {
+        let file = File::open(file_path)?;
+        let reader = BufReader::new(file);
+
+        let source = Decoder::new(reader)
+            .map_err(|e| {
+                eprintln!("Error decoding audio file: {}", e);
+                e
+            })?
+            .pausable(false)
+            .repeat_infinite();
+
+        sink.append(source);
+        sink.play();
+        if let Some(file_name) = path.file_name() {
+            let file_name_str = file_name.to_str().unwrap_or("Unknown");
+            println!("Playing: {}", file_name_str);
+        }
+    }
 
     loop {
         let mut input = String::new();
@@ -50,15 +101,12 @@ async fn play_audio(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
             "help" => {
                 println!("List of commands:");
                 println!("- help: Display this help message.");
-                // TODO: println!("- status: Display configuration");
-                // TODO: println!("- repeate: Toggle between repeate/no repeate");
+                println!("- shuffle: Toggle between shuffle/no shuffe");
                 println!("- mute: Toggle between mute/unmute");
-                // TODO: println!("- next: Play the next audio in the queue");
-                // TODO: println!("- prev: Play the previous audio in the queue");
+                println!("- skip: Skip to the next song");
+                println!("- next: Play the next audio in the queue");
                 println!("- play: Play/Unpause the audio");
                 println!("- pause: Pause the audio");
-                // TODO: println!("- fw: Go forward with 5s");
-                // TODO: println!("- bw (time): Go backward with 5s");
                 println!("- raise: Raise the volume");
                 println!("- lower: Lower the volume");
                 println!("- exit: Stop the audio player.");
@@ -94,6 +142,25 @@ async fn play_audio(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
                     sink.set_volume(sink.volume() - 0.2)
                 }
                 println!("volume: {}%", (sink.volume() * 100.0).round());
+            }
+            "next" => {
+                if is_dir {
+                    sink.skip_one();
+                    if let Some(file_name) = path.file_name() {
+                        let file_name_str = file_name.to_str().unwrap_or("Unknown");
+                        println!("Playing: {}", file_name_str);
+                    }
+                } else {
+                    println!("You can't skip");
+                }
+            }
+            "shuffle" => {
+                shuffle = !shuffle;
+                if shuffle {
+                    println!("shuffle enabled")
+                } else {
+                    println!("shuffle disabled")
+                }
             }
             "pause" => sink.pause(),
             "exit" => return Ok(()),
