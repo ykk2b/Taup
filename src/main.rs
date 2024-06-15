bfd57b87cc8a5d5c084fc67e3fbb6cfa6da8e70d
Coffee Coke <169088425+ykk2b@users.noreply.github.com>
2024-06-16 00:20:24 +0400

    added command system

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{App, Arg};
 use rodio::{Decoder, OutputStream, Sink, Source};
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{stdin, BufReader};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -35,13 +35,71 @@ async fn play_audio(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
             e
         })
         .unwrap()
-        .pausable(false);
-
-    println!("Playing {:?}s", source.total_duration().unwrap().as_secs());
+        .pausable(false)
+        .repeat_infinite();
 
     sink.append(source);
     sink.play();
-    sink.sleep_until_end();
+    println!("Type 'help' to get the list of commands. Type 'exit' to stop the player.");
 
-    Ok(())
+    loop {
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        match input.trim() {
+            "help" => {
+                println!("List of commands:");
+                println!("- help: Display this help message.");
+                // TODO: println!("- status: Display configuration");
+                // TODO: println!("- repeate: Toggle between repeate/no repeate");
+                println!("- mute: Toggle between mute/unmute");
+                // TODO: println!("- next: Play the next audio in the queue");
+                // TODO: println!("- prev: Play the previous audio in the queue");
+                println!("- play: Play/Unpause the audio");
+                println!("- pause: Pause the audio");
+                // TODO: println!("- fw: Go forward with 5s");
+                // TODO: println!("- bw (time): Go backward with 5s");
+                println!("- raise: Raise the volume");
+                println!("- lower: Lower the volume");
+                println!("- exit: Stop the audio player.");
+            }
+            "repeate" => {}
+            "mute" => {
+                if sink.volume() == 0.0 {
+                    sink.set_volume(1.0)
+                } else {
+                    sink.set_volume(0.0)
+                }
+            }
+            "play" => {
+                if sink.is_paused() {
+                    sink.play()
+                } else {
+                    sink.pause()
+                }
+            }
+            "raise" => {
+                if sink.volume() >= 0.8 {
+                    sink.set_volume(1.0)
+                } else {
+                    sink.set_volume(sink.volume() + 0.2)
+                }
+
+                println!("volume: {}%", (sink.volume() * 100.0).round());
+            }
+            "lower" => {
+                if sink.volume() <= 0.2 {
+                    sink.set_volume(0.0)
+                } else {
+                    sink.set_volume(sink.volume() - 0.2)
+                }
+                println!("volume: {}%", (sink.volume() * 100.0).round());
+            }
+            "pause" => sink.pause(),
+            "exit" => return Ok(()),
+            _ => println!("Command not recognized. Try 'help'."),
+        }
+
+        input.clear();
+    }
 }
